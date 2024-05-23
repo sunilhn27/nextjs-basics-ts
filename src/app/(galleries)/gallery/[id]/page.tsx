6fecdcaca6742bf3c05bdce575ff58901b883de8
Sunil H N <sunilh.n27@gmail.com>
2024-05-24 00:18:25 +0530

    added gallery id page

--- src/app/(galleries)/gallery/[id]/page.tsx
+++ src/app/(galleries)/gallery/[id]/page.tsx
@@ -1,27 +1,24 @@
+// Import necessary modules
 import React from "react";
-import { Photo } ,ph from "../helper"; // Assuming Photo is an array of objects
-import Image, { StaticImageData } from "next/image";
-
+import Image from "next/image";
+import { photo as photos } from "../helper"; // Import the photos array
 
 interface GalleryIDPageProps {
-  params: { id: number }; // Interface for props received by the component
+  params: { id: string };
 }
 
-function GalleryIDPage({ params }: GalleryIDPageProps): JSX.Element {
-  // Find the photo that matches the given ID (handle cases where no match found)
-  const photo: Photo = photos.find(p => p.id === id)!
-
+async function GalleryIDPage({ params }: GalleryIDPageProps) {
+  const product = photos.find((pro) => pro.id === parseInt(params.id));
+  if (!product) return <div>Product not found</div>;
   return (
     <section className="">
       <div className="flex justify-center items-center">
         <div className="text-5xl my-6">Gallery</div>
       </div>
       <div className="flex justify-center items-center">
-        <div className="w-[20rem] h-[20rem] border-2 rounded-lg ">
-          <Image src={url} alt={"alt"} height={320} width={302} />
-          <h1 className="my-4 flex items-center justify-center text-2xl font-bold">
-            {"author"}
-          </h1>
+        <div className="w-[20rem] h-[20rem] border-2 rounded-lg">
+          <Image src={product.url} alt="" width={500} height={500}></Image>
+          <h1 className="text-3xl">{product.author}</h1>
         </div>
       </div>
     </section>
