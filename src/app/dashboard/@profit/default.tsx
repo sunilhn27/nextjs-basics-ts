4a23239d14a5496b7abddd7f28c8cfa565995a6a
Sunil H N <sunilh.n27@gmail.com>
2024-05-23 23:45:16 +0530

    added dashboard

---
+++ src/app/dashboard/@profit/default.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ProfitPage() {
+  return (
+    
+      <div
+        className="w-[90%] h-[20rem] border-2 border-red-400 flex justify-center items-center shadow-xl"
+        id="revenue"
+      >
+        <div className="">
+          <h1 className="text-[2rem]">Profit</h1>
+          <p>400$</p>
+        </div>
+      </div>
+
+  );
+}
+
+export default ProfitPage;
