147dd004cc31c3968c3fdad130cab0446536cfb7
Sunil H N <sunilh.n27@gmail.com>
2024-05-28 01:36:26 +0530

    Session Wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import SessionWrapper from "@/components/SessionWrapper";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SessionWrapper>{children}</SessionWrapper>
+      </body>
     </html>
   );
 }
