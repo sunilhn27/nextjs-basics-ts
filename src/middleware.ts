b7cc88891fc3d31ea167704d9695e3014f772e29
Sunil H N <sunilh.n27@gmail.com>
2024-05-31 00:21:34 +0530

    Added Clerk Middle ware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,14 @@
-// src/middleware.ts
-import { NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export async function middleware(req: NextRequest) {
-  // Log the request URL for debugging
-  console.log('Request URL:', req.url);
 
-  // Attempt to retrieve the token
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+const isProtectedRoute = createRouteMatcher([
+    '/dashboard(.*)',
+    '/forum(.*)',
+]);
+export default clerkMiddleware(
 
-  // Log the token for debugging
-  console.log('Token:', token);
-
-  // Check if the token is present
-  if (!token) {
-    // Log the redirection for debugging
-    console.log('No token found, redirecting to signin');
-    return NextResponse.redirect(new URL('/api/auth/signin', req.url));
-  }
-
-  // Token is present, proceed to the next middleware or route handler
-  return NextResponse.next();
-}
+);
 
 export const config = {
-  matcher: ['/protected/:path*', '/gallery/:path*', '/feed/:path*'], // Protecting /protected, /gallery, and /feed routes
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
