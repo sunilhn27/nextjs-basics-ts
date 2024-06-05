f7e10ba4c83773c1df5dced59be603431ccf3c9b
Sunil H N <sunilh.n27@gmail.com>
2024-06-06 00:10:41 +0530

    added Nav Bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import Login from "@/components/Login";
-import { SignIn } from "@clerk/nextjs";
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
+      <div className="flex  items-center justify-evenly">
+        <nav className="space-x-3 p-6">
+          <Link href={"/dashboard"}>DashBoard</Link>
+
+          <Link href={"/form"}>Form</Link>
+
+          <Link href={"/userprofile"}>UserProfile</Link>
+
+          <Link href={"/about"}>About</Link>
+        </nav>
+      </div>
     </main>
   );
 }
