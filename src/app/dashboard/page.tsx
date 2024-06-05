ae4c6f96cb426fda804b48a2d8f9d5518d89ba88
Sunil H N <sunilh.n27@gmail.com>
2024-06-06 00:11:17 +0530

    added dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { auth, currentUser  } from "@clerk/nextjs/server";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 
 function DashBoardPage() {
   const useAuth = auth();
@@ -8,13 +9,15 @@ function DashBoardPage() {
   console.log(user);
   return (
     <section className="my-12">
+      <div className="flex justify-end items-center mx-6">
+        <UserButton />
+      </div>
       <div className="flex flex-col justify-center items-center mx-auto">
         <h1 className="text-5xl">Welcome to Dashboard Page</h1>
+        <p></p>
       </div>
       <div className="flex justify-center items-center py-6">
-        <div className="border-2 w-[36rem] h-[25rem] rounded-lg">
-            
-        </div>
+        <div className="border-2 w-[36rem] h-[25rem] rounded-lg"></div>
       </div>
     </section>
   );
