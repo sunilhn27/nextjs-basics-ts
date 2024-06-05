89454bda108fc85cad88717c90435a58a55ede15
Sunil H N <sunilh.n27@gmail.com>
2024-06-05 23:01:32 +0530

    Added Dashboard Page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+function DashBoardPage() {
+  const useAuth = auth();
+  const user = currentUser();
+  console.log(useAuth);
+  console.log(user);
+  return (
+    <section className="my-12">
+      <div className="flex flex-col justify-center items-center mx-auto">
+        <h1 className="text-5xl">Welcome to Dashboard Page</h1>
+      </div>
+      <div className="flex justify-center items-center py-6">
+        <div className="border-2 w-[36rem] h-[25rem] rounded-lg">
+            
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default DashBoardPage;
