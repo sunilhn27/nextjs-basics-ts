6a3115c2d28ca99110369b861be5b2f50157db28
Sunil H N <sunilh.n27@gmail.com>
2024-06-15 00:01:49 +0530

    Added Products

---
+++ src/app/(state-management)/addtocart/_data.ts
@@ -0,0 +1,8 @@
+export const Products = [
+  { id: "1", name: "Mac", price: 100 },
+  { id: "2", name: "Iphone", price: 200 },
+  { id: "3", name: "Watch", price: 300 },
+  { id: "4", name: "Ipad", price: 400 },
+  { id: "5", name: "PC", price: 500 },
+  { id: "6", name: "Server", price: 500 },
+];
