7630e85a56937cd5d3621015f224fc3ec4a6f333
Sunil H N <sunilh.n27@gmail.com>
2024-06-22 00:12:34 +0530

    changes in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
+import { ModeToggle } from "@/components/ui/mode-toggle";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <div className="flex  items-center justify-evenly">
-        <nav className="space-x-3 p-6">
-          <Link href={"/dashboard"}>DashBoard</Link>
-
-          <Link href={"/form"}>Form</Link>
-
-          <Link href={"/userprofile"}>UserProfile</Link>
-
-          <Link href={"/about"}>About</Link>
-        </nav>
-      </div>
+      
     </main>
   );
 }
